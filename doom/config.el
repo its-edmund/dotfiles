df04939fdde71e94e7b8f7b504b9c39035d94fb7
Edmund Xin <mxinburritos@gmail.com>
2025-02-27 17:47:48 -0800

    Add zellij config

--- doom/config.el
+++ doom/config.el
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-feather-dark)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -275,3 +275,5 @@
                                   cfw:fchar-top-junction ?┯
                                   cfw:fchar-top-left-corner ?┏
                                   cfw:fchar-top-right-corner ?┓)))
+
+(setq x-select-enable-clipboard nil)
